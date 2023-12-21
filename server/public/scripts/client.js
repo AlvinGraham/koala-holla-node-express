a6cb007c1c048108b9b8eb62129ccc5d98f9cb23
Alvin Graham <grah0071@gmail.com>
2023-12-21 11:47:40 -0600

    Add and test client /koalas GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,19 @@ console.log( 'js' );
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
-  
+  axios({
+    method: 'GET',
+    url: '/koalas'
+  })
+  .then((response) => {
+    console.log('/koalas GET:');
+    console.table(response.data);
+
+    renderDOM(response.data);
+  })
+  .catch((error) =>{
+    console.log('Error in /koalas GET:', error);
+  });
 } // end getKoalas
 
 function saveKoala(){
@@ -12,4 +24,20 @@ function saveKoala(){
  
 }
 
+function renderDOM (koalas) {
+  // targetable elements
+  const viewKoalasEle = document.getElementById('viewKoalas');
+
+  viewKoalasEle.innerHTML = null;
+  for (let koala of koalas) {
+    viewKoalasEle.innerHTML += `
+    <tr>
+      <td>${koala.name}</td>
+      <td>${koala.age}</td>
+      <td>${koala.gender}</td>
+      <td>${koala.ready_to_transfer}</td>
+      <td>${koala.notes}</td>
+    </tr>`;
+  }
+}
 getKoalas();
